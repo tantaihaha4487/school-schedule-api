165d4a5bf5ab7e5d5a19ade6e55ba3c0265720d1
TantaiHaha <78604116+tantaihaha4487@users.noreply.github.com>
2024-05-17 19:30:02 +0000

    Add vercel.json

--- index.js
+++ index.js
@@ -468,7 +468,7 @@ const schedule = {
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }))
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'https://login-web-front-end.vercel.app');
+    res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', '*');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
     res.setHeader('Access-Control-Allow-Credentials', true);
